fadc73c4d79847f6e15914b6c0f4a38634f892ca
727822tucs107 <154419222+727822tucs107@users.noreply.github.com>
2024-08-06 12:21:50 +0530

    Add files via upload

---
+++ Day-4/UserPanel/Logout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Logout.css';
+
+function Logout() {
+  return (
+    <div className="logout-page">
+      <h2>Logout Page</h2>
+      <p>You have been logged out successfully. Thank you for visiting.</p>
+    </div>
+  );
+}
+
+export default Logout;
