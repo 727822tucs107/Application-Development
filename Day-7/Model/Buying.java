c20c165ec8844b29923a3edc812a1633548e93c6
727822tucs107 <154419222+727822tucs107@users.noreply.github.com>
2024-08-06 10:10:27 +0530

    Add files via upload

---
+++ Day-7/Model/Buying.java
@@ -0,0 +1,25 @@
+package com.example.realestate.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Buying {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
